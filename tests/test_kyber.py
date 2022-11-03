286e545a0212b05de89216e00a0b53b1fada1fe6
Brandon Goodell <brandon.g.goodell@gmail.com>
2022-11-03 13:17:09 -0600

    test: add test_bit_rev

--- tests/test_kyber.py
+++ tests/test_kyber.py
@@ -1,9 +1,11 @@
-from crystals.kyber import _int2bytes, int2bytes, _bytes2int, bytes2int
+from crystals.kyber import _int2bytes, int2bytes, _bytes2int, bytes2int, _bit_rev
+from random import getrandbits
 import pytest
 from random import randbytes
 
 
 SAMPLE_SIZE: int = 2**10
+LOG_SAMPLE_SIZE: int = 10
 
 
 def test_int2bytes():
@@ -44,3 +46,28 @@ def test_bytes2int():
         bytes2int(x=0.001)
 
     assert bytes2int(x=b'00000000000000111') == 7
+
+
+def test_bytes2int_and_int2bytes_are_inverses():
+    for i in range(SAMPLE_SIZE):
+        assert bytes2int(int2bytes(x=i, length=LOG_SAMPLE_SIZE)) == i
+
+    for i in range(SAMPLE_SIZE):
+        next_bytes_object = bytes(0)
+        for j in range(LOG_SAMPLE_SIZE):
+            next_bit = getrandbits(1)
+            if next_bit:
+                next_bytes_object += b'1'
+            else:
+                next_bytes_object += b'0'
+        assert int2bytes(x=bytes2int(x=next_bytes_object), length=LOG_SAMPLE_SIZE) == next_bytes_object
+
+def test_bit_rev():
+    for i in range(SAMPLE_SIZE):
+        x = int2bytes(x=i, length=LOG_SAMPLE_SIZE)
+        reversed_i = _bit_rev(x=i, length=LOG_SAMPLE_SIZE)
+        reversed_x = int2bytes(x=reversed_i, length=LOG_SAMPLE_SIZE)
+        assert len(x) == len(reversed_x) == LOG_SAMPLE_SIZE
+        for i in range(LOG_SAMPLE_SIZE):
+            assert x[i] == reversed_x[LOG_SAMPLE_SIZE - 1 - i]
+        print()
