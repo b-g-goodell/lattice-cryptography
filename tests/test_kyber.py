e6a77e65e1b9b0ed7d51c0f681cb17a3aad278ee
Brandon Goodell <brandon.g.goodell@gmail.com>
2022-11-07 11:06:02 -0700

    test: add test_parse_many

--- tests/test_kyber.py
+++ tests/test_kyber.py
@@ -1,4 +1,4 @@
-from crystals.kyber import Q, _int2bytes, int2bytes, _bytes2int, bytes2int, _bit_rev, bit_rev, is_pow_two, _bit_rev_cp, bit_rev_cp, _reduce, reduce, _round_up, round_up, N, LOG_Q, _parse_one
+from crystals.kyber import Q, _int2bytes, int2bytes, _bytes2int, bytes2int, _bit_rev, bit_rev, is_pow_two, _bit_rev_cp, bit_rev_cp, _reduce, reduce, _round_up, round_up, N, LOG_Q, _parse_one, _parse_many, K
 from random import getrandbits, randrange
 import pytest
 from math import ceil, log2
@@ -210,15 +210,40 @@ def test_parse_one():
 
         test_integers_to_bytes += [first_byte, second_byte, third_byte]
 
-        observed_integer_one = first_byte + 256 * (second_byte % 16)
-        observed_integer_two = (second_byte // 16) + 16 * third_byte
-
-        assert observed_integer_one == test_integers[i]
-        assert observed_integer_two == test_integers[i+1]
-
+    resulting_ints: list[int]
+    index: int
     resulting_ints, index = _parse_one(x=bytes(test_integers_to_bytes))
     expected_ints = [_ for _ in test_integers if _ < Q]
     expected_ints = expected_ints[:N]
+    assert len(resulting_ints) == N
+    assert all(isinstance(x, int) for x in resulting_ints)
+    assert all(0 <= x < Q for x in resulting_ints)
     for i, (next_result_int, next_test_int) in enumerate(zip(resulting_ints, expected_ints)):
         assert next_result_int == next_test_int
 
+
+def test_parse_many():
+    test_integers = [randrange(2**LOG_Q) for _ in range(10*K*K*N)]
+    test_integers_to_bytes = []
+    for i in range(0, len(test_integers)-1, 2):
+        first_byte = test_integers[i] % 256
+
+        tmp = test_integers[i+1] % 16
+        third_byte = (test_integers[i+1] - tmp)//16
+
+        second_byte_mod_16 = (test_integers[i] - first_byte)//256
+        second_byte_div_by_16_rd_down = test_integers[i+1] - 16*third_byte
+        second_byte = second_byte_mod_16 + 16*second_byte_div_by_16_rd_down
+
+        test_integers_to_bytes += [first_byte, second_byte, third_byte]
+
+    resulting_ints: list[list[list[int]]] = _parse_many(x=bytes(test_integers_to_bytes))
+    # We only check shape, size, and modulus because we call _parse_one multiple times to make _parse_many
+    assert isinstance(resulting_ints, list)
+    assert len(resulting_ints) == K
+    assert all(isinstance(x, list) for x in resulting_ints)
+    assert all(len(x) == K for x in resulting_ints)
+    assert all(isinstance(y, list) for x in resulting_ints for y in x)
+    assert all(len(y) == N for x in resulting_ints for y in x)
+    assert all(isinstance(z, int) for x in resulting_ints for y in x for z in y)
+    assert all(0 <= z < Q for x in resulting_ints for y in x for z in y)
